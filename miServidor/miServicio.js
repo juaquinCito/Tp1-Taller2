55e5cb83ebaa2c023154aa78309cecf5c6666099
Ernesto Gimenez <gmx90PT@gmail.com>
2024-09-10 23:29:20 -0300

    txt,number, redicion del filter

--- miServidor/miServicio.js
+++ miServidor/miServicio.js
@@ -1,19 +1,19 @@
 import fs from "fs"
 const siguienteArray = [2, 10, "a", 4, "b", 6, "d", true, "e", 9, 1, "z", 12, "r", "c", false]
-const condicion = 5
+const condicion = "number";
 
 function filtrar(siguienteArray, condicion) {
     // filtramos los `numbers` y si ya estan incluidos en el array a devolver
     const arrayFiltrado = siguienteArray.filter(posicion =>
-        typeof (condicion) == typeof (posicion) && !arrayFiltrado.include(posicion))
+         typeof (posicion) == condicion  && !arrayFiltrado.include(posicion))
     //finalmente ordenamos el array de menor a mayor a traves del metodo sort
     const arrayOrdenado = arrayFiltrado.sort((a, b) => a - b)
-    if(condicion != null && condicion != undefined){
+    if (condicion != null && condicion != undefined) {
         const transformado = arrayOrdenado.join('\n')
-        const transformarTxT = fs.writeFile('archivo.txt', transformado, (err) => {
-            if (err) throw err });
-        return transformarTxT      
-    }else{
+        const transformarTxT = fs.writeFile('archivoArray.txt', transformado, (err) => {
+            if (err) throw err
+        });
+    } else {
         return `ERROR!`
     }
 }
