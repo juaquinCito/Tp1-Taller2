1254e5b3ec76f9cac19787aa678a16acf899777b
Ernesto Gimenez <gmx90PT@gmail.com>
2024-09-10 21:12:03 -0300

    1er prototipoAcorregir

---
+++ miServidor/miServicio.js
@@ -0,0 +1,22 @@
+import fs from "fs"
+const siguienteArray = [2, 10, "a", 4, "b", 6, "d", true, "e", 9, 1, "z", 12, "r", "c", false]
+const condicion = 5
+
+function filtrar(siguienteArray, condicion) {
+    // filtramos los `numbers` y si ya estan incluidos en el array a devolver
+    const arrayFiltrado = siguienteArray.filter(posicion =>
+        typeof (condicion) == typeof (posicion) && !arrayFiltrado.include(posicion))
+    //finalmente ordenamos el array de menor a mayor a traves del metodo sort
+    const arrayOrdenado = arrayFiltrado.sort((a, b) => a - b)
+    const aDevolver = (condicion != null && condicion != undefined) ? transformarTxT(arrayOrdenado) : `Error!`
+    return arrayFiltrado
+}
+
+function transformarTxT(arrayOrdenado) {
+    const transformado = arrayOrdenado.join('\n');
+    const transformarTxT = fs.writeFile('archivo.txt', transformado, (err) => {
+        if (err) throw err
+    });
+    return transformarTxT
+}
+
